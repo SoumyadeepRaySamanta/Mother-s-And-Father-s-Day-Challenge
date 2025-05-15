9420a60356a89c8804cdf7e2670632abf1ef38c7
Soumyadeep Ray Samanta <159132811+SoumyadeepRaySamanta@users.noreply.github.com>
2025-05-15 18:36:30 +0530

    Delete Math Based Problem/Missing Number.py

--- Math Based Problem/Missing Number.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        total = n * (n + 1) // 2
-        Elements_sum = sum(nums)
-        return total - Elements_sum
